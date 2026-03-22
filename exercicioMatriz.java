7ea90f518cc32277142aa5d568e8ae8fd91d1d52
JefersonRodrigues21 <dev.geekjs@gmail.com>
2026-03-22 06:58:20 -0300

    herança

--- exercicioMatriz.java
+++ exercicioMatriz.java
@@ -1,5 +1,5 @@
 public class exercicioMatriz {
     public static void main(string[] args){
-        int[][] matriz = { {9,8,7}, {5,3,2}, {6,6,7} }
+        int[][] matriz = { {9,8,7}, {5,3,2}, {6,6,7} };
     }
 }
