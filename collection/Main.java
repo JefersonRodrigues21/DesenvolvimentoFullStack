be71db95edd5be7374aeee526bc26e4aa1f182c0
JefersonRodrigues21 <dev.geekjs@gmail.com>
2026-04-07 13:40:27 -0300

    Collection explicativo

--- collection/Main.java
+++ collection/Main.java
@@ -1,11 +1,11 @@
-package collection;
+package collection; // Definindo o pacote collection para organizar as classes relacionadas a coleções
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.ArrayList; // Importando a classe ArrayList do pacote java.util
+import java.util.Iterator; // Importando a classe Iterator do pacote java.util para iterar sobre os elementos da coleção
 
 class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> numeros = new ArrayList<>();
+        ArrayList<Integer> numeros = new ArrayList<>(); // Criando uma instância de ArrayList para armazenar objetos do tipo Integer
         numeros.add(1);
         numeros.add(3);
         numeros.add(2);
@@ -13,13 +13,13 @@ class Main {
         System.out.println("ArrayList: " + numeros);
 
         // Criando uma instancia de Iterator
-        Iterator<Integer> it = numeros.iterator();
+        Iterator<Integer> it = numeros.iterator(); // O método iterator() retorna um objeto do tipo Iterator que pode ser usado para iterar sobre os elementos da coleção
 
-        int numero = it.next();
+        int numero = it.next(); // O método next() retorna o próximo elemento da coleção e avança o cursor para o próximo elemento. Se não houver mais elementos, ele lança uma NoSuchElementException.
         System.out.println("Elemento: " + numero);
 
-        while(it.hasNext()){
-            it.forEachRemaining((value) -> System.out.println(value + ","));
+        while(it.hasNext()){ // O método hasNext() retorna true se houver mais elementos para iterar, caso contrário, retorna false. Ele é usado para controlar o loop de iteração.
+            it.forEachRemaining((value) -> System.out.println(value + ",")); // O método forEachRemaining() é um método do Iterator que executa a ação especificada para cada elemento restante na coleção. Ele recebe um objeto do tipo Consumer, que é uma interface funcional que representa uma operação que aceita um único argumento e não retorna nenhum resultado. Neste caso, estamos usando uma expressão lambda para imprimir cada valor seguido de uma vírgula.
         }
     }
 }
