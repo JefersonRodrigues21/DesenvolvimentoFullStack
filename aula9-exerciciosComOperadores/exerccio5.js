b473fa0cc46d6391681f282e214dcff33db11f10
JefersonRodrigues21 <dev.geekjs@gmail.com>
2026-04-27 16:18:48 -0300

    Exercício de média ponderada

---
+++ aula9-exerciciosComOperadores/exerccio5.js
@@ -0,0 +1,10 @@
+//Faça um algoritmo que leia quatro números informados pelo usuário e que depois imprima a MÉDIA PONDERADA, sabendo-se que os pesos são respectivamente: 1, 2, 3 e 4.
+
+let n1 = 5;
+let n2 = 4;
+let n3 = 7;
+let n4 = 8;
+let media;
+
+media = (n1*1 + n2*2 + n3*3 + n4*4)/10;
+console.log(`A média ponderada é: ${media}`);
