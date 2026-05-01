fd4ef7fe524f9827b30407a8ccb560ca6ce860cd
JefersonRodrigues21 <dev.geekjs@gmail.com>
2026-05-01 00:00:05 -0300

    Usando forEach, for in, for of

---
+++ aula10-ifSwitchForWhile/for.js
@@ -0,0 +1,46 @@
+// for, for/in, for/of
+let carros = [
+  { id: 1, modelo: "Corsa", marca: "Chevrolet", preco: 45000, cod: 478156 },
+  { id: 2, modelo: "Punto", marca: "Fiat", preco: 120000, cod: 478161 },
+];
+let total, carro;
+
+for (let i = 0; i < carros.length; i++) {
+  total += carros[i].preco;
+}
+
+for (let i in carro) {
+  total += carros[i].preco;
+}
+
+for (let carro of carros) {
+  total += carro.preco;
+}
+
+console.log('-----------------------------------------------------------------------');
+
+let a = [10, 20, 30, 40, 50, 60];
+
+a.forEach((valor) => console.log(valor));
+
+let tot = 0;
+a.forEach((valor) => {
+  tot += valor;
+});
+
+console.log(tot);
+
+a.forEach(function (valor, indice, array) {
+  console.log(array[indice]);
+});
+
+console.log("-------------------------------------------------------------");
+
+let car = [
+  { marca: "Volvo", modelo: "Xc 60", ano: 2026 },
+  { marca: "Audi", modelo: "Audi A3", ano: 2026 },
+];
+
+for (let característica in car) {
+  console.log(car[característica]);
+}
