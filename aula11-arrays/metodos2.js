0ada21a8739e66e2ba18668949c154c1d86f811a
JefersonRodrigues21 <dev.geekjs@gmail.com>
2026-05-06 23:36:38 -0300

    Método Map (array.map).

--- aula11-arrays/metodos2.js
+++ aula11-arrays/metodos2.js
@@ -46,23 +46,60 @@
 // console.log('-------------------------------------------------------------------');
 
 //? Desafio
-let produtos = [
-    {id: 1, descricao: 'SmartPhone', categoria: 'Eletrônico'},
-    {id: 2, descricao: 'Notebook', categoria: 'Eletrônico'},
-    {id: 3, descricao: 'Geladeira', categoria: 'Eletrodoméstico'},
-    {id: 4, descricao: 'Liquidificador', categoria: 'Eletrodoméstico'}
-];
-let selecao = produtos.filter(
-    function(item) {
-        return item.categoria === 'Eletrônico';
-    }
-);
-console.log(selecao);
+// let produtos = [
+//     {id: 1, descricao: 'SmartPhone', categoria: 'Eletrônico'},
+//     {id: 2, descricao: 'Notebook', categoria: 'Eletrônico'},
+//     {id: 3, descricao: 'Geladeira', categoria: 'Eletrodoméstico'},
+//     {id: 4, descricao: 'Liquidificador', categoria: 'Eletrodoméstico'}
+// ];
+// let selecao = produtos.filter(
+//     function(item) {
+//         return item.categoria === 'Eletrônico';
+//     }
+// );
+// console.log(selecao);
 
-let selecao2 = produtos.filter(
-    function(item) {
-        return item.categoria === 'Eletrodoméstico';
-    }
-);
-console.log(selecao2);
-console.log('---------------------------------------------------------------------');
+// let selecao2 = produtos.filter(
+//     function(item) {
+//         return item.categoria === 'Eletrodoméstico';
+//     }
+// );
+// console.log(selecao2);
+// console.log('---------------------------------------------------------------------');
+
+//? Método MAP (Array.map()) - permite a você iterar sobre o array e modificar seus elementos usando uma função de callback. A função callback será execuratada em cada um dos elementos do array.
+// let arr = [3, 4, 5, 6];
+// let modifiedArr = arr.map(function(element){
+//     return element * 3;
+// });
+// console.log(modifiedArr);
+
+// // ou pode fazer assim
+
+// let arr2 = [3, 4, 5, 6];
+
+// for (let i = 0; i < arr2.length; i++) {
+//     arr2[i] = arr2[i] * 3;
+// };
+// console.log(arr2);
+// console.log('--------------------------------------------------------------------------------');
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+ 
+// let multip = numbers.map(function(valor){
+//     return valor * 2;
+// });
+// console.log(multip);
+
+// // Outra forma de usar o MAP
+
+// let funcionarios = [
+//     {nome: 'Luiz', idade: 62},
+//     {nome: 'David', idade: 22},
+//     {nome: 'Arthur', idade: 18},
+//     {nome: 'Lucas', idade: 40},
+// ];
+// nomes = funcionarios.map(pessoa => pessoa.nome);
+// console.log(nomes);
+
+//?
