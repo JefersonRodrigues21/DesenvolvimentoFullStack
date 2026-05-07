1a6c56356016d0cba00a6221d60b00626b2a83de
JefersonRodrigues21 <dev.geekjs@gmail.com>
2026-05-06 23:10:53 -0300

    Método Array.filter.

---
+++ aula11-arrays/metodos2.js
@@ -0,0 +1,68 @@
+//? Método filter(), recebe cada elemento do array e aplica uma instrução condicional a ele. Se essa condição for verdadeira, o elemento é colocado no array de resultado. Se for falsa, o elemento não é colocado lá.
+// let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let resultado = numeros.filter((item) => item % 2 === 0); // Inline
+// console.log(resultado);
+
+// console.log("--------------------------------------------------------------------------");
+
+// let numerosFiltrados = numeros.filter(function (valor) {
+//   // função
+//   return valor > 5;
+// });
+// console.log(numerosFiltrados);
+// console.log("--------------------------------------------------------------------------");
+
+// function buscarValores(valor) {
+//   return valor < 5;
+// }
+// let numerosEncontrados = numeros.filter(buscarValores);
+// console.log(numerosEncontrados);
+// console.log("--------------------------outra-forma-de usar-----------------------------");
+
+// let r1 = numeros.filter((valor) => {
+//   return valor > 5;
+// });
+// console.log(r1);
+// console.log("--------------------------outra-forma-de usar-----------------------------");
+
+// var r2 = numeros.filter(valor => valor > 5);
+// console.log(r2);
+// console.log("--------------------------------------------------------------------------");
+
+// let funcionarios = [
+//     {nome: 'Luiz', idade: 62},
+//     {nome: 'Davi', idade: 22},
+//     {nome: 'Arthur', idade: 62},
+//     {nome: 'Lucas', idade: 40},
+// ]
+
+// let pessoasListagem = funcionarios.filter(
+//     function(valor) {
+//         return valor.nome.length < 5
+//     }
+// );
+// console.log(pessoasListagem);
+// console.log('-------------------------------------------------------------------');
+
+//? Desafio
+let produtos = [
+    {id: 1, descricao: 'SmartPhone', categoria: 'Eletrônico'},
+    {id: 2, descricao: 'Notebook', categoria: 'Eletrônico'},
+    {id: 3, descricao: 'Geladeira', categoria: 'Eletrodoméstico'},
+    {id: 4, descricao: 'Liquidificador', categoria: 'Eletrodoméstico'}
+];
+let selecao = produtos.filter(
+    function(item) {
+        return item.categoria === 'Eletrônico';
+    }
+);
+console.log(selecao);
+
+let selecao2 = produtos.filter(
+    function(item) {
+        return item.categoria === 'Eletrodoméstico';
+    }
+);
+console.log(selecao2);
+console.log('---------------------------------------------------------------------');
